2aa23fd504967bd84ecb68e636edb20831609dda
Kendra Roberson <kendra.roberson@verizon.com>
2019-06-26 18:07:32 -0500

    Update to Job Roles code

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $('#name').focus(); // Cursor automatically appears in the Name: input area when
 $('#other-title').hide(); // hides the text field. I want it hidden until 'Other' is clicked.
 
 $('#title').click(function() { // I need a formula here that will target the value or text of the 'Other' option and unhide the text area when clicked.
-    $('other-title').show();
+    $('#other-title').show();
 });
 
 
