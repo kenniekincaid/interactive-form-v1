d74fc76238250631ba07468abb3e2809ee84a557
Kendra Roberson <kendra.roberson@verizon.com>
2019-07-02 17:44:35 -0500

    Cleaned up some things

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-//Set Focus on the first text field section:
-$('#name').focus(); //Focuses cursor to Name text field on page load.
+$(document).ready(function() {//Codes will run when JS script is ready to execute.
+
+$('#name').focus(); //Set Focus on the first text field section:
 
 //"Job Role" section:
 $('#other-title').hide(); //Initially hide the job role for Javascript. It will dispaly if JS is disabled.
@@ -15,86 +16,17 @@ $('#title').change(function() { //Function and event handler to control when tex
 //"T-shirt Info" section:
 $('#design option:first').hide(); //hides the 'Select Theme' option from drop-down menu.
 $('#color option').hide(); //hides all other options from view.
+// $('#color').hide();//Extra credit to hide color field until designs are chosen.
 
-$('#design').change(function() {
-
-    $('#design option:selected').val();
-
-    console.log($('#design option:selected').val());
-
+$('#design').change(function() {//function with event to take place when design is selected/changed.
+    $('#design option:selected').val();//gets the value of the design options.
     $('#color option').hide(); //hides all other options from view.
-
-
     if ($('#design option:selected').val() === 'js puns') {
-        //show t-shirts with js puns
-        $('#color option:contains("JS Puns shirt only")').show();
+        $('#color option:contains("JS Puns shirt only")').show(); //show t-shirts with js puns
     } else {
-        // show t-shirts with heart js
-        $('#color option:contains("JS shirt only")').show();
+        $('#color option:contains("JS shirt only")').show();// show t-shirts with heart js
     }
-
-    // const selectDesign = document.getElementById("design");
-    // const selectColor = document.getElementById("color");
-    // const $punColors = $('#color option:contains(JS Puns shirt only)');
-    // const $heartColors = $('#color option:contains(JS shirt only)');
-
-    // console.log(selectDesign.options.length);
-    // console.log(selectDesign);
-    // for (var i = 0; i < selectDesign.options.length; i++) {
-    //     // if (selectDesign.options[i].selected = true) {
-    //     //     $(punColors).show();
-    //     // } else {
-    //     //     $(punColors).hide();
-    //     } if ('#design text'==='JS Puns shirt only') {
-    //         $('$punColors').show();
-    //     } else {
-    //         $($punColors).hide();
-    //     } if ('#design text'=='JS shirt only') {
-    //         $($heartColors).show();
-    //     } else {
-    //         $($heartColors).hide();
-    //     }
 }); 
-
-
-
-
-
-// $.each(results['hide'], function(name, title) {                     
-//     $(document.getElementById('edit-field-service-sub-cat-value').options).each(function(index, option) {
-//       if( option.value == title ) {
-//         option.hidden = true; // not fully compatible. option.style.display = 'none'; would be an alternative or $(option).hide();
-//       }
-//     });
-// });
-
-
-
-
-
-//Brainstorming below:
-// $('#design').change(function() {
-
-//     const $shirtDesign = $('#design').val();
-//     const $shirtColor = $('#color').val();
-//     // const jspunsColors = $('#color').val();
-//     // const heartjsColors = $('#color').val();
-//         if ($(this).val()=='js puns') {
-//             $('#selectlist')[0-2].show();
-//             $('#selectlist')[3-5].hide();
-//             // $('#color').hide('tomato', 'steelblue', 'dimgrey');
-//             // $('#color').show('cornflowerblue', 'darkslategrey', 'gold').hide('tomato', 'steelblue', 'dimgrey');
-//             // $('#color').val('tomato','steelblue','dimgrey').hide();
-//         } else {
-//         if ($(this).val()=='heart js') {
-//             $('#selectlist')[0,1,2].hide();
-//             $('#selectlist')[3,4,5].show();
-//             // $('#color').hide('cornflowerblue', 'darkslategrey', 'gold');  
-//                  // $('#color').show('tomato', 'steelblue', 'dimgrey').hide('cornflowerblue', 'darkslategrey', 'gold');
-//                 // $('#color').val('cornflowerblue','darkslategrey','gold').hide();
-//             }
-//         }
-// }).trigger('change');
     
 //"Register for Activities" section:
 
@@ -104,3 +36,6 @@ $('#design').change(function() {
 
 //Form validation messages:
 
+
+
+});
