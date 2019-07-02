254188664e660e20d507d8651618ef93496fb779
Kendra Roberson <kendra.roberson@verizon.com>
2019-07-02 16:53:24 -0500

    Added T-shirt functionality

--- js/script.js
+++ js/script.js
@@ -13,29 +13,88 @@ $('#title').change(function() { //Function and event handler to control when tex
 });//.trigger('change');
 
 //"T-shirt Info" section:
+$('#design option:first').hide(); //hides the 'Select Theme' option from drop-down menu.
+$('#color option').hide(); //hides all other options from view.
+
+$('#design').change(function() {
+
+    $('#design option:selected').val();
+
+    console.log($('#design option:selected').val());
+
+    $('#color option').hide(); //hides all other options from view.
+
+
+    if ($('#design option:selected').val() === 'js puns') {
+        //show t-shirts with js puns
+        $('#color option:contains("JS Puns shirt only")').show();
+    } else {
+        // show t-shirts with heart js
+        $('#color option:contains("JS shirt only")').show();
+    }
+
+    // const selectDesign = document.getElementById("design");
+    // const selectColor = document.getElementById("color");
+    // const $punColors = $('#color option:contains(JS Puns shirt only)');
+    // const $heartColors = $('#color option:contains(JS shirt only)');
+
+    // console.log(selectDesign.options.length);
+    // console.log(selectDesign);
+    // for (var i = 0; i < selectDesign.options.length; i++) {
+    //     // if (selectDesign.options[i].selected = true) {
+    //     //     $(punColors).show();
+    //     // } else {
+    //     //     $(punColors).hide();
+    //     } if ('#design text'==='JS Puns shirt only') {
+    //         $('$punColors').show();
+    //     } else {
+    //         $($punColors).hide();
+    //     } if ('#design text'=='JS shirt only') {
+    //         $($heartColors).show();
+    //     } else {
+    //         $($heartColors).hide();
+    //     }
+}); 
+
+
+
+
+
+// $.each(results['hide'], function(name, title) {                     
+//     $(document.getElementById('edit-field-service-sub-cat-value').options).each(function(index, option) {
+//       if( option.value == title ) {
+//         option.hidden = true; // not fully compatible. option.style.display = 'none'; would be an alternative or $(option).hide();
+//       }
+//     });
+// });
+
+
+
+
 
 //Brainstorming below:
-$('#design').change(function() {
-    const $shirtDesign = $('#design').val();
-    const $shirtColor = $('#color').val();
-    // const jspunsColors = $('#color').val();
-    // const heartjsColors = $('#color').val();
-        if ($(this).val()=='js puns') {
-            $('#selectlist')[0,1,2].show();
-            $('#selectlist')[3,4,5].hide();
-            // $('#color').hide('tomato', 'steelblue', 'dimgrey');
-            // $('#color').show('cornflowerblue', 'darkslategrey', 'gold').hide('tomato', 'steelblue', 'dimgrey');
-            // $('#color').val('tomato','steelblue','dimgrey').hide();
-        } else {
-        if ($(this).val()=='heart js') {
-            $('#selectlist')[0,1,2].hide();
-            $('#selectlist')[3,4,5].show();
-            // $('#color').hide('cornflowerblue', 'darkslategrey', 'gold');  
-                 // $('#color').show('tomato', 'steelblue', 'dimgrey').hide('cornflowerblue', 'darkslategrey', 'gold');
-                // $('#color').val('cornflowerblue','darkslategrey','gold').hide();
-            }
-        }
-}).trigger('change');
+// $('#design').change(function() {
+
+//     const $shirtDesign = $('#design').val();
+//     const $shirtColor = $('#color').val();
+//     // const jspunsColors = $('#color').val();
+//     // const heartjsColors = $('#color').val();
+//         if ($(this).val()=='js puns') {
+//             $('#selectlist')[0-2].show();
+//             $('#selectlist')[3-5].hide();
+//             // $('#color').hide('tomato', 'steelblue', 'dimgrey');
+//             // $('#color').show('cornflowerblue', 'darkslategrey', 'gold').hide('tomato', 'steelblue', 'dimgrey');
+//             // $('#color').val('tomato','steelblue','dimgrey').hide();
+//         } else {
+//         if ($(this).val()=='heart js') {
+//             $('#selectlist')[0,1,2].hide();
+//             $('#selectlist')[3,4,5].show();
+//             // $('#color').hide('cornflowerblue', 'darkslategrey', 'gold');  
+//                  // $('#color').show('tomato', 'steelblue', 'dimgrey').hide('cornflowerblue', 'darkslategrey', 'gold');
+//                 // $('#color').val('cornflowerblue','darkslategrey','gold').hide();
+//             }
+//         }
+// }).trigger('change');
     
 //"Register for Activities" section:
 
