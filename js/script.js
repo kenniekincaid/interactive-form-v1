2127b4d271c5660bf8c98732c05a6b275817da56
Kendra Roberson <kendra.roberson@verizon.com>
2019-07-07 05:22:21 -0500

    Pt 1 of Payment section working...

--- js/script.js
+++ js/script.js
@@ -16,92 +16,91 @@ $('#title').change(function() { //Function and event handler to control when tex
 //"T-shirt Info" section:
 $('#design option:first').hide(); //hides the 'Select Theme' option from drop-down menu.
 $('#color option').hide(); //hides all other options from view.
-// $('#color').hide();//Extra credit to hide color field until designs are chosen.
+$('#colors-js-puns').hide();//Extra credit to hide color field until designs are chosen.
 
 $('#design').change(function() {//function with event to take place when design is selected/changed.
     $('#design option:selected').val();//gets the value of the design options.
     $('#color option').hide(); //hides all other options from view.
     if ($('#design option:selected').val() === 'js puns') {
+        $('#colors-js-puns').show();//extra credit for color field to be hidden until design selected.
         $('#color option:contains("JS Puns shirt only")').show(); //show t-shirts with js puns
-        $('#color').val("cornflowerblue");
+        $('#color').show().val("cornflowerblue");
     } else {
+        $('#colors-js-puns').show();//extra credit for color field to be hidden until design selected.
         $('#color option:contains("JS shirt only")').show();// show t-shirts with heart js
-        $('#color').val("tomato");
+        $('#color').show().val("tomato");
     }
 }); 
 
 //"Register for Activities" section:
-let totalCostSpan = $('.activities').append('<span id="totalCost">Total Cost:</span>');//Created total cost DOM element.
-$('.activities').append($domELement); //Appends global element after the last child nested under activities.
+let totalCostSpan = $('.activities').append('<span id="totalCost"></span>');//Created total cost DOM element nd appended after last child.
+$('.activities').append(totalCostSpan);
+// $('#totalCost').hide();
 
-$('.activities :checkbox').on('change', function() {
-// $('.activities :checkbox').on('change', function() {
-    let totalActivityCost = 0; //Global variable to store total activity cost, initially set to 0.
-    let parentTextContent = $(this)[0].parentElement.innerText();
-    // let parentTextContent = $('.activities label').innerText(); //mine
-    let dollarIndex = parentLabelText.indexOf('$'); //focuses on the values containing a $ sign.
-    let activityCost = parentTextContent.slice(dollarIndex + 1);
-    // let inputElement = $('.activities :checkbox').val('$'); // to find the text content to index
-
-        if ($(this)[0].checked) {
-            totalActivityCost += parseInt(activityCost);
-        } else {
-            totalActivityCost += parseInt(activityCost);
-        }
-    // for(let i=0; i < activities.length; i++) {//loops over all of the checkboxes
-    //     let activity = activities[i];
-    //     let activityText = activity.parentElement.innerText; //references the text of the parent element
-    //     let dollarValue = parseInt(activityText.slice(activityText.indexOf('$') + 1)); //Finds value AFTER the $ sign and grabs the numbers after it.
-    // if ($('input[name=' + activity.name + ']').prop('checked')) {
-    //     totalSum += dollarValue;
-    // }
-    // }
-    let spanTotalActivity = $('#totalCost'); //assigns the total cost to the span variable.
-    spanTotalActivity.text("Total: $" + totalActivityCost); //adds text before the total cost.
-});   
-
-
-// $('.activities').change(function() {
-//     let totalActivityCost = 0; //Global variable to store total activity cost - initially set to 0.
-//     let $activities = $('.activities input[type=checkbox]');
-//         for(let i=0; i < activities.length; i++) {//loops over all of the checkboxes
-//             let activity = activities[i];
-//             let activityText = activity.parentElement.innerText; //references the text of the parent element
-//             let dollarValue = parseInt(activityText.slice(activityText.indexOf('$') + 1)); //Finds value AFTER the $ sign and grabs the numbers after it.
-//         if ($('input[name=' + activity.name + ']').prop('checked')) {
-//             totalSum += dollarValue;
-//         }
-//         }
-//     let totalCostSpan = $('#totalCost');
-//     totalCostSpan.text("Total: $" + totalActivityCost);
-
-// let parentText = 
-// let activities
-    
-   
+// let totalActivityCost = 0;
+// $('.activities input:checkbox').change(function () {
+//     let inputElement = '';
+//     let parentTextContent = '';
+//     let dollarIndex = '$';
+//     let activityCost = '.slice()';
+//     if ('inputElement :checked') {
+//         //add he cost of the curently selected activity totalCost
+//     }else{
+//         //subtract the cost
+//     }
+// });
 
 
 //"Payment Info" section: HINT - it's just like to T-shirt section:
-// $('#design option:first').hide(); //hides the 'Select Theme' option from drop-down menu.
-// $('#color option').hide(); //hides all other options from view.
-// // $('#color').hide();//Extra credit to hide color field until designs are chosen.
+$('#payment').show().val("credit card");//credit card is the default view.
+$('#payment option:contains("Select Payment Method")').hide(); //hides the 'Select Payment Method' option from drop-down menu.
+// $('#payment option:contains("PayPal")').hide();
+// $('#payment option:contains("Bitcoin")').hide();
+$('div p:contains("PayPal")').hide(); //hides the paypal section until that payment method is selected.
+$('div p:contains("Bitcoin")').hide();//hides the bitcoin section until that payment method is selected.
 
-// $('#design').change(function() {//function with event to take place when design is selected/changed.
-//     $('#design option:selected').val();//gets the value of the design options.
-//     $('#color option').hide(); //hides all other options from view.
-//     if ($('#design option:selected').val() === 'js puns') {
-//         $('#color option:contains("JS Puns shirt only")').show(); //show t-shirts with js puns
-//     } else {
-//         $('#color option:contains("JS shirt only")').show();// show t-shirts with heart js
+$('#payment').change(function() {
+    const $paymentOptions = $(this).val();
+    if ($paymentOptions === "credit card") {
+        $('#credit-card').show();
+        $('div p:contains("PayPal")').hide();
+        $('div p:contains("Bitcoin")').hide();
+    } else if ($paymentOptions === "paypal") {
+        $('div p:contains("PayPal")').fadeIn(700).show();
+        $('div p:contains("Bitcoin")').hide();
+        $('#credit-card').hide();
+    } else if ($paymentOptions === "bitcoin") {
+        $('div p:contains("Bitcoin")').fadeIn(700).show();
+        $('#credit-card').hide();
+        $('div p:contains("PayPal")').hide();
+    }
+});
+
+//VALIDATION ERRORS:
+
+
+
+//ALTERNATE PAYMENT RESOLUTION:
+// $('#payment').change(function() {
+//     const $paymentOptions = $(this).val();
+//     // if ($paymentOptions === "credit card") {
+//     //     // $('#payment option').hide().val("paypal");
+//     //     // $('#payment option').hide().val("bitcoin");
+//     if ($paymentOptions === "paypal") {
+//         $('#payment option').show().val("paypal");
+//         $('#payment option').hide().val("bitcoin");
+//         $('#payment option').hide().val("credit card");
+//         $('#credit-card').hide();
+//     } else if ($paymentOptions === "bitcoin") {
+//         $('#payment option').show().val("bitcoin");
+//         $('#payment option').hide().val("credit card");
+//         $('#credit-card').hide();
+//         $('#payment option').hide().val("paypal");
 //     }
-// }); 
-
+// });
 
 //Form validation section:
 
-
 //Form validation messages:
-
-
 
 });
